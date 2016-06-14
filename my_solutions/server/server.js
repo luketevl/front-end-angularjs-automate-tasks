5516cf98089263f583c261bd67267a724b1717f6
Lucas Henrique <luketevl@gmail.com>
2016-06-14 01:50:36 -0300

    Commit server and API
    Created the server and API method that return all peoples in NODEJS

--- my_solutions/server/server.js
+++ my_solutions/server/server.js
@@ -1,8 +1,18 @@
 'use strict';
 
-// Importing the module HTTP
-const http = require('http');
+// Creating base routes
+const express = require('express');
 
+// Creating APP
+const app = express();
 
-// Listen the SERVER
-http.listen(595,  => console.log('http://localhost:595'));
+// ROUTE for API
+app.get('/api/v1/people', (req, res) => {
+  const peoples = require('./models/Peoples')();
+
+  // How we are using JSON the response use the function json
+  res.json(peoples);
+});
+
+// Creating and Listening the SERVER
+app.listen(595, () => console.log('http://localhost:595'));
